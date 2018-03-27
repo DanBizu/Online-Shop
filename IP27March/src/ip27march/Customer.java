007bb1e115568a9d632e0022a9c917942de9a790
Mada Lina <jitaru_mada@yahoo.com>
2018-03-27 13:20:03 +0300

    Implement of Customer class

--- IP27March/src/ip27march/Customer.java
+++ IP27March/src/ip27march/Customer.java
@@ -6,17 +6,23 @@ public class Customer {
   private String customerName;
   private String username;
   private String password;
-  private ShoppingCart myShoppingCart;
 
-
-  public Customer register() {
-  return null;
+  public Customer(final String name, final String usernm, final String pass) {
+    this.register(name, usernm, pass);
   }
 
-  public void changePassword() {
+  public Customer register(final String name, final String usernm, final String pass) {
+    customerName = name;
+    username = usernm;
+    password = pass;
   }
 
-  public void login() {
+  public void changePassword(final String newPassword) {
+    password = newPassword;
+  }
+
+  public boolean login(final String pass) {
+    return pass==password;
   }
 
 }
